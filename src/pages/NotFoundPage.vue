6abeae54d706e12d1162532a4187d313cc5aa870
GuglielmoBini <guglielmobini@gmail.com>
2023-03-16 17:39:52 +0100

    added NotFoundPage, changed tag a in router-link in AppHeader

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'NotFoundPage'
+}
+</script>
+
+<template>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h1>404 | Not Found</h1>
+        <router-link class="btn btn-danger mt-3" to='/'>Indietro</router-link>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    height: calc(100vh - 56px);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // importo le pagine che voglio usare
 import HomePage from "../pages/HomePage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
 
 //definisco il router e le rotte
 const router = createRouter({
   history: createWebHistory(),
+  linkExactActiveClass: "active",
   routes: [
     { path: "/", name: "home", component: HomePage },
-    { path: "/:pathMatch(.*)*", redirect: "/" },
+    { path: "/:pathMatch(.*)*", component: NotFoundPage },
   ],
 });
 
