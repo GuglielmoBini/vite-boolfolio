9a5193fcd823481cce0752125a286cb12b72dc03
GuglielmoBini <guglielmobini@gmail.com>
2023-03-17 16:56:33 +0100

    corrections, added home button, added technologies

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -23,15 +23,19 @@ export default {
 
 <template>
     <div class="d-flex flex-column align-items-center">
-        <div class="my-5" :class="{ 'card': project.image_url }">
+        <div class="my-5" :class="{ 'card': isDetail && project.image_url }">
             <div class="row g-0 justify-content-center">
-                <div v-if="project.image_url" class="col-4">
+                <div v-if="isDetail && project.image_url" class="col-4">
                     <img :src='project.image_url' class="img-fluid w-100 h-100 rounded-start" :alt='project.name'>
                 </div>
-                <div class="col-8" :class="{ 'card': !project.image_url }">
+                <div class="col-8" :class="{ 'card': !isDetail || !project.image_url }">
                     <div class="row g-0 h-100 flex-column">
                         <div class="card-body d-flex flex-column">
-                            <h3 class="card-title">{{ project.name }}</h3>
+                            <div class="d-flex align-items-center justify-content-between mb-3">
+                                <h3 class="card-title">{{ project.name }}</h3>
+                                <router-link v-if="!isDetail" class="btn btn-outline-primary"
+                                    :to="{ name: 'project-details', params: { id: project.id } }">Dettagli</router-link>
+                            </div>
                             <p class="card-text flex-grow-1">{{ project.description }}</p>
                             <p class="card-text">
                                 <small class="text-muted">
@@ -43,17 +47,23 @@ export default {
                                     <strong>Type:</strong> {{ project.type.label }}
                                 </small>
                             </p>
+                            <ul v-if="isDetail" class="p-0 m-0">
+                                <li v-if="project.technologies.length > 0" v-for="technology in project.technologies"
+                                    class="badge rounded-pill me-2" :class="`bg-${technology.color}`">
+                                    {{ technology.label }}
+                                </li>
+                                <div v-else> - </div>
+                            </ul>
                         </div>
                         <div class="card-body border-top d-flex align-items-center justify-content-around">
                             <a :href='project.project_url'>Project link</a>
                             <a v-if="project.image_url" :href='project.image_url'>Image URL</a>
-                            <router-link v-if="!isDetail" class="btn btn-warning"
-                                :to="{ name: 'project-details', params: { id: project.id } }">Dettagli</router-link>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <router-link v-if="isDetail" class="btn btn-danger w-25" :to="{ name: 'home' }">Home</router-link>
     </div>
 </template>
 
